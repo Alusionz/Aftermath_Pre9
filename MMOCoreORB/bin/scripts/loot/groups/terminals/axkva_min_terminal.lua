a736ee28c6ef62a62630e9d01db40bc8c18e6f3c
Alusionz <122238077+Alusionz@users.noreply.github.com>
2025-05-23 15:32:29 -0700

    Update axkva_min_terminal.lua

--- MMOCoreORB/bin/scripts/loot/groups/terminals/axkva_min_terminal.lua
+++ MMOCoreORB/bin/scripts/loot/groups/terminals/axkva_min_terminal.lua
@@ -6,8 +6,8 @@ axkva_min_terminal = {
 		{itemTemplate = "armor_attachments", weight = 1600000},
 		{itemTemplate = "clothing_attachments", weight = 1600000},
 		{itemTemplate = "nightsister_common", weight = 2700000},
-		{itemTemplate = "power_crystals", weight = 2000000},
-		{itemTemplate = "named_color_crystals", weight = 2100000}
+		--{itemTemplate = "power_crystals", weight = 2000000},
+		{itemTemplate = "color_crystals", weight = 4100000}
 	}
 }
 
